554f26acb9bac5773947520614d7cc37fb9e3e11
@mzadante <mzadantemartinez@gmail.com>
2023-10-11 13:04:54 -0300

    cambios minimos

--- js/mostrarContenido.js
+++ js/mostrarContenido.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var contenido = localStorage.getItem('contenidoTinyMCE');
+    console.log(contenido);
     if (contenido) {
         document.getElementById('contenidoAqui').innerHTML = 
         contenido + `
